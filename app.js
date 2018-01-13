6dd0b4174ec4f40e965c4ed6cca4b667207d53bc
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-12 20:47:54 -0800

    set up 1st 2 event listeners.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("connected");
+
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('top');
+
+function handleEnter() {
+
+
+}
+
+function handleLeave() {
+
+
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleLeave));
